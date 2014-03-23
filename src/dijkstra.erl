3fe7448dd471f57e49784686b3dd7a24d3ddf4ef
Moritz <moritz@tibidat.com>
2014-03-23 17:11:02 +0100

    Fixes the implementation for Dijkstra's algorithm.

--- src/dijkstra.erl
+++ src/dijkstra.erl
@@ -2,64 +2,81 @@
 
 -export([dijkstra/2,
          dijkstra/3,
-         prepare/2,
+         print_path/3,
          dummy/0]).
 
 dijkstra(Graph, Source) ->
-    Vertices = digraph:vertices(Graph),
-    {Dist, Prev} = prepare(Vertices, Source),
-    iterate(Graph, Vertices, Dist, Prev).
+    dijkstra(Graph, Source, full).
 
-dijkstra(_Graph, _Source, _Target) ->
-    ok.
+dijkstra(Graph, Source, Target) ->
+    {Vertices, Dist, Prev} = prepare(Graph, Source),
+    iterate(Graph, Vertices, Dist, Prev, Target).
 
-iterate(_Graph, [], Dist, _Prev) ->
-    Dist;
-iterate(Graph, Q, Dist, Prev) ->
-    % find the closest next vertex
-    case array:foldl(fun(Idx, E, {CurMinIdx, CurMin}) ->
-                             Check = lists:member(Idx, Q),
-                             case erlang:min(E, CurMin) of
-                                 E when Check -> {Idx, E};
-                                 _ -> {CurMinIdx, CurMin}
+%% TODO: improve the output
+print_path(Distances, Hops, Target) ->
+    print_path(Distances, Hops, Target, []).
+print_path(_Distances, _Hops, start, Route) ->
+    Route;
+print_path(Distances, Hops, Target, Route) ->
+    Prev = array:get(Target, Hops),
+    print_path(Distances, Hops, Prev, [Prev|Route]).
+
+iterate(_Graph, [], Dist, Prev, _Target) ->
+    {Prev, Dist};
+iterate(Graph, UnvisitedVertices, Dist, Prev, Target) ->
+    case lists:foldl(fun(Vertex, {ClosestVertex, Distance}) ->
+                             D = array:get(Vertex, Dist),
+                             case erlang:min(D, Distance) of
+                                 D -> {Vertex, D};
+                                 _ -> {ClosestVertex, Distance}
                              end
-                     end, {undefined, infinity}, Dist) of
-        {undefined, infinity} ->
-            % no futher vertices reachable from the source
-            iterate(Graph, [], Dist, Prev);
-        {U, _} ->
-            NQ = lists:delete(U, Q),
-            Edges = digraph:out_edges(Graph, U),
-            {NewDist, NewPrev} = check_neighbours(Graph, Edges, U, Dist, Prev),
-            iterate(Graph, NQ, NewDist, NewPrev)
+                     end, {undefined, infinity}, UnvisitedVertices) of
+        {undefined, infinity} -> %% dead end
+            iterate(Graph, [], Dist, Prev, Target);
+        {Target, _} -> %% reached target vertex
+            iterate(Graph, [], Dist, Prev, Target);
+        {NextVertex, _} ->
+            NQ = lists:delete(NextVertex, UnvisitedVertices),
+            Edges = digraph:out_edges(Graph, NextVertex),
+            {NewDist, NewPrev} = check_neighbours(Graph, Edges,
+                                                  NQ, Dist, Prev),
+            iterate(Graph, NQ, NewDist, NewPrev, Target)
     end.
+
 
 check_neighbours(_Graph, [], _, Dist, Prev) ->
     {Dist, Prev};
-check_neighbours(Graph, [Edge|RemEdges], U, Dist, Prev) ->
-    {_Name, _From, V, Weight} = digraph:edge(Graph, Edge),
-    CurDist = array:get(V, Dist),
-    NDist = array:get(U, Dist),
-    case NDist of
-        infinity ->
-            check_neighbours(Graph, RemEdges, U, Dist, Prev);
-        _ ->
-            case NDist + Weight of
-                D when D < CurDist ->
-                    NewDist = array:set(V, D, Dist),
-                    NewPrev = array:set(V, U, Prev),
-                    check_neighbours(Graph, RemEdges, U, NewDist, NewPrev);
+check_neighbours(Graph, [Edge|RemEdges], UnvisitedVertices, Dist, Prev) ->
+    {_Name, From, To, Weight} = digraph:edge(Graph, Edge),
+    case lists:member(To, UnvisitedVertices) of
+        true ->
+            CurDist = array:get(To, Dist),
+            %% TODO refactor this to make it DRYer
+            case array:get(From, Dist) of
+                infinity ->
+                    check_neighbours(Graph, RemEdges, UnvisitedVertices,
+                                     Dist, Prev);
+                D when (D+Weight < CurDist) ->
+                    NewDist = array:set(To, D+Weight, Dist),
+                    NewPrev = array:set(To, From, Prev),
+                    check_neighbours(Graph, RemEdges, UnvisitedVertices,
+                                     NewDist, NewPrev);
                 _ ->
-                    check_neighbours(Graph, RemEdges, U, Dist, Prev)
-            end
+                    check_neighbours(Graph, RemEdges, UnvisitedVertices,
+                                     Dist, Prev)
+            end;
+        false ->
+            check_neighbours(Graph, RemEdges, UnvisitedVertices, Dist, Prev)
     end.
 
-prepare(Vertices, Source) ->
-    Count = length(Vertices),
-    D = array:new(Count, {default, infinity}),
+prepare(Graph, Source) ->
+    Count = digraph:no_vertices(Graph),
+    D = array:new([{size, Count}, fixed, {default, infinity}]),
     Dist = array:set(Source, 0, D),
-    Prev = array:new(Count, {default, undefined}),
-    {Dist, Prev}.
+    P = array:new([{size, Count}, fixed, {default, undefined}]),
+    Prev = array:set(Source, start, P),
+    UnvisitedVertices = digraph:vertices(Graph),
+    {UnvisitedVertices, Dist, Prev}.
 
 dummy() ->
     G = digraph:new(),
@@ -67,9 +84,16 @@ dummy() ->
     digraph:add_vertex(G, 1),
     digraph:add_vertex(G, 2),
     digraph:add_vertex(G, 3),
-    digraph:add_edge(G, 0, 0, 1, 3),
-    digraph:add_edge(G, 1, 0, 2, 5),
-    digraph:add_edge(G, 2, 1, 3, 15),
-    digraph:add_edge(G, 3, 1, 2, 1),
-    digraph:add_edge(G, 4, 3, 1, 6),
+    digraph:add_vertex(G, 4),
+    digraph:add_vertex(G, 5),
+    digraph:add_vertex(G, 6),
+    digraph:add_edge(G, 0, 1, 3),
+    digraph:add_edge(G, 0, 3, 5),
+    digraph:add_edge(G, 1, 2, 2),
+    digraph:add_edge(G, 1, 6, 4),
+    digraph:add_edge(G, 2, 4, 6),
+    digraph:add_edge(G, 4, 3, 4),
+    digraph:add_edge(G, 5, 6, 2),
+    digraph:add_edge(G, 5, 2, 8),
+    digraph:add_edge(G, 6, 3, 1),
     G.
